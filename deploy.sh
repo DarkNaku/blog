d7032e86da8978ed308bb4dbb85d05be95dc9551
skyrack <skyrack@naver.com>
2022-01-06 10:46:55 +0900

    delete test

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,6 @@
 
 echo -e "\033[0;32mDeploying Blog...\033[0m"
 
-git add .
-
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-
-git commit -m "$msg"
-
-git push origin master
-
 hugo -t hugo-coder
 
 cd public
@@ -26,3 +15,17 @@ fi
 git commit -m "$msg"
 
 git push origin master
+
+cd..
+
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+git commit -m "$msg"
+
+git push origin master
+
